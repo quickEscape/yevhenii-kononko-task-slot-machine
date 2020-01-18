0a2ca7f3184f0f1f3c98b12eef029c4a9aa48a0b
Evgeniy Kononko <evgeniy.escape@gmail.com>
2020-01-18 03:22:46 +0200

    upload build

---
+++ build/precache-manifest.d409c8f0fb558bf9a92e870d39d57ae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20e5f1d9c449418800bc61ba58fa6e38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5de305d871f5fefe1892",
+    "url": "/static/css/main.f3e19773.chunk.css"
+  },
+  {
+    "revision": "c5bef2af5f67177b5008",
+    "url": "/static/js/2.61b7972b.chunk.js"
+  },
+  {
+    "revision": "28ac78fef8675bfe412d94015e05d458",
+    "url": "/static/js/2.61b7972b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5de305d871f5fefe1892",
+    "url": "/static/js/main.a0aaa4f9.chunk.js"
+  },
+  {
+    "revision": "3fe5dd4b6a79eddd5a93",
+    "url": "/static/js/runtime-main.675951c8.js"
+  }
+]);
